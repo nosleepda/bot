e3ca691b37d950cfe475e8159e2c02416e73e106
Konstantin Spitsin <konstantin.spicin@simbirsoft.com>
2023-01-22 22:35:56 +0300

    help command

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -30,14 +30,27 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, "You wrote: "+update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-			_, err := bot.Send(msg)
-			if err != nil {
-				log.Fatal(err)
+			switch update.Message.Command() {
+			case "help":
+				helpCommand(bot, update.Message)
+			default:
+				defaultBehavior(bot, update.Message)
 			}
 		}
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, "You wrote: "+inputMessage.Text)
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "help")
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+
+	bot.Send(msg)
 }
